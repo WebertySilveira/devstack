c6bbc0ec18ea52553175b61b1a5685cd972f89a8
webertySilveira <weberty.silveira.sousa@gmail.com>
2024-09-23 21:42:47 -0300

    first commit

---
+++ src/index.php
@@ -0,0 +1,13 @@
+<?php
+$host = 'db';
+$dbname = 'mydb';
+$username = 'user';
+$password = 'userpass';
+
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connected to MySQL successfully!";
+} catch (PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+}
